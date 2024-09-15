b143e3301e2648f3940efd2a9cd98fe5989a8502
Timur Stupin <hache03@gmail.com>
2024-09-15 16:43:58 +0300

    logger added

--- src/main/java/ru/timur/Main.java
+++ src/main/java/ru/timur/Main.java
@@ -5,32 +5,37 @@ import com.fastcgi.FCGIInterface;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.Properties;
+import java.util.logging.*;
 
 public class Main {
     private static final String HTTP_RESPONSE = "Content-Type: application/json\nContent-Length: %d\n\n%s";
-    private static FileWriter logger;
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) throws IOException {
         var fcgiInterface = new FCGIInterface();
-        logger = new FileWriter("log.txt");
+
+        FileHandler fileHandler = new FileHandler("CGI.log", true);
+        fileHandler.setFormatter(new SimpleFormatter());
+        logger.addHandler(fileHandler);
+        logger.setLevel(Level.ALL);
 
         while (fcgiInterface.FCGIaccept() >= 0) {
             Properties request = FCGIInterface.request.params;
 
             if (!request.getProperty("REQUEST_METHOD").equals("GET")) {
-                logger.write(String.format("Received request type %s\n", request.getProperty("REQUEST_METHOD")));
-                logger.flush();
+                logger.warning(String.format("Received request type %s", request.getProperty("REQUEST_METHOD")));
                 continue;
             }
 
             String requestString = request.getProperty("QUERY_STRING");
-            logger.write(String.format("Received request: %s\n", requestString));
-            logger.flush();
+            logger.info(String.format("Received request: %s", requestString));
 
             String answer = getAnswer(requestString);
+            logger.info(String.format("Calculated answer for request: %s", answer));
             
             System.out.printf(HTTP_RESPONSE, answer.getBytes(StandardCharsets.UTF_8).length, answer);
+            logger.info("Answer sent");
         }
-        logger.close();
     }
     
     private static String getAnswer(String requestString) throws IOException {
@@ -38,8 +43,7 @@ public class Main {
         try {
             requestData = RequestData.parseRequestData(requestString);
         } catch (RequestDataParseException e){
-            logger.write(e.getMessage());
-            logger.flush();
+            logger.severe(e.getMessage());
             return getJSONMessage(StateCode.ERROR, false);
         }
         return getJSONMessage(StateCode.OK, checkIntersection(requestData));
