9737ade2deedb139219f8dd13dca248e5d9b65f7
Timur Stupin <hache03@gmail.com>
2024-09-17 13:25:18 +0300

    R input type changed

--- static/main.js
+++ static/main.js
@@ -4,9 +4,7 @@ window.addEventListener("load", loadTableData);
 const message_type = Object.freeze({
     OK: 1,
     EMPTY_FIELDS: 2,
-    R_NOT_DIGIT: 3,
-    INVALID_R: 4,
-    SOME_SERVER_ERROR: 5
+    SOME_SERVER_ERROR: 3
 })
 
 //Имитация radio используя checkbox
@@ -71,12 +69,6 @@ function show_user_message(message){
         case message_type.EMPTY_FIELDS:
             alert("Пожалуйста заполните все поля!");
             break;
-        case message_type.R_NOT_DIGIT:
-            alert("Значение R должно быть числом!");
-            break;
-        case message_type.INVALID_R:
-            alert("Значение R должно быть в пределах [-5; 5]!");
-            break;
         case message_type.SOME_SERVER_ERROR:
             alert("Упс... Произошла ошибка при работе с сервером. Пожалуйста, повторите попытку позже.");
             break;
@@ -86,8 +78,6 @@ function show_user_message(message){
 //Функция валидации данных формы
 function validate_data(x, y, r){
     if(x == null || y == null || r == null ||  r === "") return message_type.EMPTY_FIELDS;
-    if(isNaN(r) || isNaN(parseFloat(r))) return message_type.R_NOT_DIGIT
-    if(r < -5 || r > 5) return message_type.INVALID_R
     return message_type.OK;
 }
 
