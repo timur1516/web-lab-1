542bb26d581e1a11197b6f62aad8263b407dc2c5
Timur Stupin <hache03@gmail.com>
2024-09-28 20:00:23 +0300

    bug fix

--- src/main/java/ru/timur/Main.java
+++ src/main/java/ru/timur/Main.java
@@ -4,7 +4,6 @@ import com.fastcgi.FCGIInterface;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.time.LocalDateTime;
 import java.util.Properties;
 import java.util.logging.*;
 
@@ -12,27 +11,16 @@ public class Main {
     private static final String HTTP_RESPONSE = "Status: %s\nContent-Type: application/json\nContent-Length: %d\n\n%s";
     private static final Logger logger = Logger.getLogger(Main.class.getName());
 
-    private static long startTime;
-
     public static void main(String[] args) throws IOException {
         var fcgiInterface = new FCGIInterface();
 
-        FileHandler fileHandler = new FileHandler("CGI.log", true);
-        fileHandler.setFormatter(new SimpleFormatter());
-        logger.addHandler(fileHandler);
         logger.setLevel(Level.ALL);
+        logger.info("Server started");
 
         while (fcgiInterface.FCGIaccept() >= 0) {
-            startTime = System.currentTimeMillis();
             Properties request = FCGIInterface.request.params;
-
-            String requestString = request.getProperty("QUERY_STRING");
-            logger.info(String.format("Received request: %s", requestString));
-
             try {
-                String answer = getAnswer(requestString);
-                logger.info(String.format("Calculated answer for request: %s", answer));
-                sendResponse(Status.OK, answer);
+                processRequest(request);
             } catch (RequestDataParseException e) {
                 logger.severe(e.getMessage());
                 sendResponse(Status.BAD_REQUEST, "");
@@ -40,20 +28,25 @@ public class Main {
         }
     }
 
+    private static void processRequest(Properties request) throws RequestDataParseException {
+        long startTime = System.nanoTime() / 1000;
+
+        String requestString = request.getProperty("QUERY_STRING");
+        logger.info(String.format("Received request: %s", requestString));
+        RequestData requestData = RequestData.parseRequestData(requestString);
+        boolean hit = checkIntersection(requestData);
+
+        long executionTime = System.nanoTime() / 1000 - startTime;
+
+        String response = String.format("{\"hit\": %b, \"executionTime\": %d}", hit, executionTime);
+        logger.info(String.format("Calculated answer for request: %s", response));
+
+        sendResponse(Status.OK, response);
+    }
+
     private static void sendResponse(Status status, String content){
         System.out.printf(HTTP_RESPONSE, status, content.getBytes(StandardCharsets.UTF_8).length, content);
         logger.info("Answer sent");
-    }
-    
-    private static String getAnswer(String requestString) throws RequestDataParseException {
-        RequestData requestData = RequestData.parseRequestData(requestString);
-        return getJSONMessage(checkIntersection(requestData));
-    }
-
-    private static String getJSONMessage(boolean hit){
-        long endTime = System.currentTimeMillis();
-        long executionTime = endTime - startTime;
-        return String.format("{\"hit\": %b, \"executionTime\": %d}", hit, executionTime);
     }
 
     private static boolean checkIntersection(RequestData requestData) {
--- static/main.js
+++ static/main.js
@@ -1,6 +1,7 @@
+// Авто сохранение при обновлении страницы
 let tableData = [];
 window.addEventListener("load", loadTableData);
-//Подобие enum для обработки исключений и вывода сообщений
+// Подобие enum для обработки исключений и вывода сообщений
 const message_type = Object.freeze({
     OK: 1,
     EMPTY_FIELDS: 2,
@@ -8,9 +9,10 @@ const message_type = Object.freeze({
     SOME_SERVER_ERROR: 4
 })
 
+// Значение X (храним отдельно так как авто обновляется при вводе)
 let x_value = 0;
 
-//Бинарный ввод X ;)
+// Бинарный ввод X ;)
 const x_checkboxes = document.getElementsByName("x_checkbox_input");
 x_checkboxes.forEach(checkbox => {checkbox.addEventListener("change", () => {
     let str = "0000"
@@ -20,7 +22,7 @@ x_checkboxes.forEach(checkbox => {checkbox.addEventListener("change", () => {
     x_value = parseInt(str, 2);
     if(x_value >= 8) x_value -= 16;
     document.getElementById("x_value_label").textContent = x_value;
-})})
+})});
 
 //Функция отправки запроса на сервер и получения ответа
 async function submitForm(event) {
@@ -30,17 +32,15 @@ async function submitForm(event) {
     //Извлекаем данные формы
     const formData = new FormData(event.target);
     const x = x_value
-    const y = formData.get("y_radio_input");
-    const r = formData.get("r_text_input");
+    const y = formData.get("y_text_input");
+    const r = formData.get("r_radio_input");
 
     //Проводим валидацию
     let result = validate_data(x, y, r);
-    if(result !== message_type.OK){
-        show_user_message(result);
-        return;
-    }
+    show_user_message(result);
+    if(result !== message_type.OK) return;
 
-    //Выполняем запрос измеряя время
+    //Выполняем запрос
     const queryParams = new URLSearchParams();
     queryParams.append("X", x);
     queryParams.append("Y", y);
@@ -54,44 +54,50 @@ async function submitForm(event) {
             return;
         }
         responseData = await response.json();
+
+        //Записываем данные в историею
+        let execution_time = responseData.executionTime;
+        let hit = responseData.hit;
+        add_data_to_history(x, y, r, hit, execution_time);
+
     } catch (error){
         show_user_message(message_type.SOME_SERVER_ERROR);
-        return;
     }
-
-    //Записываем данные в историию
-    let execution_time = responseData.executionTime;
-    let hit = responseData.hit;
-    add_data_to_history(x, y, r, hit, execution_time);
 }
 
-//Метод для показа сообшений пользователю
+// Метод генерации сообщений пользователю
 function show_user_message(message){
+    let error_field = document.getElementById("error_field");
+    error_field.style.visibility = "visible";
     switch (message){
         case message_type.EMPTY_FIELDS:
-            alert("Пожалуйста заполните все поля!");
+            error_field.textContent = "Пожалуйста заполните все поля!";
             break;
         case message_type.WRONG_X:
-            alert("Значение X должно быть от -3 до 5");
+            error_field.textContent = "Значение X должно быть от -3 до 5";
             break;
         case message_type.SOME_SERVER_ERROR:
-            alert("Упс... Произошла ошибка при работе с сервером. Пожалуйста, повторите попытку позже.");
+            error_field.textContent = "Упс... Произошла ошибка при работе с сервером. Пожалуйста, повторите попытку позже.";
+            break;
+        default:
+            error_field.style.visibility = "hidden";
+            error_field.textContent = "";
             break;
     }
 }
 
-//Функция валидации данных формы
+// Функция валидации данных формы
 function validate_data(x, y, r){
-    if(x == null || y == null || r == null ||  r === "") return message_type.EMPTY_FIELDS;
+    if(x == null || y == null || r == null ||  y === "") return message_type.EMPTY_FIELDS;
     if(x < -3 || x > 5) return message_type.WRONG_X;
     return message_type.OK;
 }
 
-//Функция добавления данных в таблицу
+// Функция добавления данных в таблицу
 function add_data_to_history(x, y, r, hit, execution_time){
     let table_ref = document.querySelector("#history_table tbody");
 
-    let newRow = table_ref.insertRow(-1);
+    let newRow = table_ref.insertRow(0);
 
     let xCell = newRow.insertCell(0);
     let yCell = newRow.insertCell(1);
@@ -102,10 +108,10 @@ function add_data_to_history(x, y, r, hit, execution_time){
 
     let xText = document.createTextNode(x.toString());
     let yText = document.createTextNode(y.toString());
-    let rText = document.createTextNode(Number(r).toFixed(2).toString());
+    let rText = document.createTextNode(r.toString());
     let hitText = document.createTextNode(hit.toString());
     let timeText = document.createTextNode(new Date().toLocaleTimeString());
-    let executionTimeText = document.createTextNode(execution_time.toString() + "мс");
+    let executionTimeText = document.createTextNode(execution_time.toString() + "мкс");
 
     tableData.push([xText.textContent, yText.textContent, rText.textContent, hitText.textContent, timeText.textContent, executionTimeText.textContent]);
     sessionStorage.setItem("tableData", JSON.stringify(tableData));
@@ -124,7 +130,7 @@ function loadTableData() {
     if (savedData) {
         tableData = JSON.parse(savedData);
         const table = document.querySelector("#history_table tbody");
-        tableData.forEach(row => {
+        tableData.reverse().forEach(row => {
             let newRow = table.insertRow(-1);
             row.forEach(element => {
                 let newCell = newRow.insertCell(-1);
